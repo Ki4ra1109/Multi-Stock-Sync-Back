613ac7ee45d6da6c5b0a54b081f08f93c4ec71e4
astronautmarkus <astronautmarkus@gmail.com>
2025-01-07 15:09:53 -0300

    feat: Make SKU field nullable in productos table migration

--- multistock-backend/database/migrations/2025_01_07_102000_create_productos_table.php
+++ multistock-backend/database/migrations/2025_01_07_102000_create_productos_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
-            $table->string('sku')->unique(); // Agregado SKU
+            $table->string('sku')->unique()->nullable(); //Unique SKU
             $table->foreignId('tipo')->constrained('tipo_productos')->onDelete('cascade');
             $table->foreignId('marca')->constrained('marcas')->onDelete('cascade');
             $table->boolean('control_stock');
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-    
+
     /**
      * Reverse the migrations.
      */
