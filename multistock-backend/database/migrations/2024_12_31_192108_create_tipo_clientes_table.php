529bd5429010fbb88f1ac75cfbc6758cd3cc689d
astronautmarkus <astronautmarkus@gmail.com>
2025-01-07 10:54:22 -0300

    feat: Seed initial data for tipo_clientes table with 'Empresa' and 'Persona'

--- multistock-backend/database/migrations/2024_12_31_192108_create_tipo_clientes_table.php
+++ multistock-backend/database/migrations/2024_12_31_192108_create_tipo_clientes_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -15,7 +16,12 @@ return new class extends Migration
             $table->id(); // 'id' field with type bigint unsigned
             $table->string('tipo'); // empresa/persona
             $table->timestamps();
-        });        
+        });
+        
+        DB::table('tipo_clientes')->insert([
+            ['tipo' => 'Empresa'],
+            ['tipo' => 'Persona'],
+        ]);
     }
 
     /**
