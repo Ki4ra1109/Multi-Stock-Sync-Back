717de8e5874344ed084667ca9fed92860dd00d42
Ki4ra1109 <ki.rubio@duocuc.cl>
2025-02-14 15:02:24 -0300

    WIP

--- multistock-backend/app/Models/Product.php
+++ multistock-backend/app/Models/Product.php
@@ -1,13 +1,15 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SyncStatus extends Model
+class Product extends Model
 {
     use HasFactory;
 
-    protected $table = 'sync_status';
-    protected $fillable = ['status', 'start_time', 'end_time', 'total_products', 'processed_products'];
+    protected $table = 'products';
+
+    protected $fillable = ['nombre', 'precio', 'stock'];
 }
--- multistock-backend/database/migrations/2025_02_13_183918_create_products_table.php
+++ multistock-backend/database/migrations/2025_02_13_183918_create_products_table.php
@@ -9,13 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre'); // Nombre del producto
+            $table->decimal('precio', 10, 2); // Precio con dos decimales
+            $table->integer('stock'); // Stock disponible
             $table->timestamps();
         });
-    }
+    }    
 
     /**
      * Reverse the migrations.
