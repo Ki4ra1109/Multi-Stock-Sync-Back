86d363a17294350d91aa24977159be4637cc60e8
Ki4ra1109 <ki.rubio@duocuc.cl>
2025-03-03 12:11:09 -0300

    fix: correct class name casing for ReviewController

--- multistock-backend/app/Http/Controllers/MercadoLibre/Reportes/ReviewController.php
+++ multistock-backend/app/Http/Controllers/MercadoLibre/Reportes/ReviewController.php
@@ -6,7 +6,7 @@ use App\Models\MercadoLibreCredential;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 
-class ReviewController
+class reviewController
 {
     /**
      * Get reviews from MercadoLibre API using client_id.
