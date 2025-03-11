e84b396e2f13af17044fd1174ebab090a2ff6770
Ki4ra1109 <ki.rubio@duocuc.cl>
2025-03-11 10:25:45 -0300

    refactor: remove pagination from listProductsByClientId and add getStockController

---
+++ multistock-backend/app/Http/Controllers/MercadoLibre/Products/getStockController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\MercadoLibre\Products;
+
+use App\Models\MercadoLibreCredential;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+
+class getStockController{
+
+
+}
