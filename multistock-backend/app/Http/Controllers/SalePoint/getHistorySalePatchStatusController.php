3ce06ef428b5ca135b850fc7a488e7381db6359b
Monmonsalve <isaac.monsalve.m@gmail.com>
2025-05-08 12:57:44 -0400

    Refactor validation logic in generatedSaleNote and getHistorySalePatchStatus controllers to simplify type_emission handling and ensure optional fields are correctly validated.

--- multistock-backend/app/Http/Controllers/SalePoint/getHistorySalePatchStatusController.php
+++ multistock-backend/app/Http/Controllers/SalePoint/getHistorySalePatchStatusController.php
@@ -11,11 +11,7 @@ class getHistorySalePatchStatusController{
 
     public function getHistorySalePatchStatus(Request $request, $saleId, $status){
         try{
-            $type_emission = $request->input('type_emission');
-
-            // Validar el tipo de emisión
-            if ($type_emission === 'boleta') {
-                //Validar los datos de entrada para boleta
+            //Validar los datos de entrada para boleta
                 $validateData = $request->validate([
                     'warehouse_id' => 'required|integer|exists:warehouses,id',
                     'client_id' => 'required|integer|exists:clientes,id',
@@ -23,8 +19,9 @@ class getHistorySalePatchStatusController{
                     'amount_total_products' => 'required|integer',
                     'price_subtotal' => 'required|integer',
                     'price_final' => 'required|integer',
-                    'type_emission' => 'required|string|max:255',
+                    'type_emission' => 'nullable|string|max:255',
                     'observation' => 'nullable|string|max:255',
+                    'name_companies' => 'nullable|string|max:255',
                 ]);
 
                 $validateData["products"] = json_encode($validateData["products"]);
@@ -32,32 +29,6 @@ class getHistorySalePatchStatusController{
 
                 $actualizarVenta = Sale::where('id', $saleId)->update($validateData);
                 $vista = Sale::where('id', $saleId)->first();   
-
-            } else if ($type_emission === 'factura') {
-                //Validar los datos de entrada para factura
-                $validateData = $request->validate([
-                    'warehouse_id' => 'required|integer|exists:warehouses,id',
-                    'client_id' => 'required|integer|exists:clientes,id',
-                    'products' => 'required|array',
-                    'amount_total_products' => 'required|integer',
-                    'price_subtotal' => 'required|integer',
-                    'price_final' => 'required|integer',
-                    'type_emission' => 'required|string|max:255',
-                    'observation' => 'nullable|string|max:255',
-                    'name_companies' => 'required|string|max:255',
-                ]);
-
-                $validateData["products"] = json_encode($validateData["products"]);
-                $validateData["status_sale"] = $status;
-
-                $actualizarVenta = Sale::where('id', $saleId)->update($validateData);
-
-                $vista = Sale::where('id', $saleId)->first();
-
-            } else {
-                // Respuesta de error si el tipo de emisión no es válido
-                return response()->json(['message' => 'Tipo de emisión no válido.'], 422);
-            }
 
             // Respuesta exitosa
             return response()->json([
