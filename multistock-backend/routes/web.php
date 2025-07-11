90a66b26211d307dda99489db1579207475a9b2b
Daniel_OV <daos209@gmail.com>
2025-07-11 10:58:10 -0400

    add email verification functionality and resend verification email endpoint

--- multistock-backend/routes/web.php
+++ multistock-backend/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -18,3 +20,20 @@ use Maatwebsite\Excel\Facades\Excel;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect('/?verified=1');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('message', 'Correo de verificación reenviado!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
